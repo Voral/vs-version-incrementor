cd4376f57ffddb63f95ff8639231cdbd190f8c30
Alexander Vorobyev <voral.adm@gmail.com>
2025-03-19 18:34:43 +0300

    build: Configure version increment

---
+++ .vs-version-increment.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Vasoft\VersionIncrement\Config;
+
+return (new Config())
+    ->setSection('chore', 'Other changes', hidden: true)
+    ->setSection('style', 'Code style', hidden: true)
+    ->setSection('refactor', 'Refactor', hidden: true)
+    ->setSection('ci', 'CI', hidden: true)
+    ->setSection('build', 'Build', hidden: true);
